93b71fa4e724fc75e400293da49bfedd92719c7b
TminTtapcode <oakceniki@gmail.com>
2026-07-28 00:16:46 +0700

    Delete ecommerce directory

--- ecommerce/src/main/java/com/ecommerce/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.service;
-
-import com.ecommerce.dto.request.LoginRequestDTO;
-import com.ecommerce.dto.response.LoginResponseDTO;
-
-public interface AuthService {
-    LoginResponseDTO login(LoginRequestDTO request);
-}
