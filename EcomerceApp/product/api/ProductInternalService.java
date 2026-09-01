6de5cc8434c6237ceab2ef59893c51bbe2295b40
TminTtapcode <oakceniki@gmail.com>
2026-09-01 12:37:33 +0700

    Delete EcomerceApp/product directory

--- EcomerceApp/product/api/ProductInternalService.java
+++
@@ -1,8 +0,0 @@
-package com.tmt.ecommerce.product.api;
-
-import com.tmt.ecommerce.product.api.dto.ProductVariantInfoDto;
-
-public interface ProductInternalService {
-    // Chỉ trả về DTO, tuyệt đối không trả về Entity ProductVariant
-    ProductVariantInfoDto getVariantInfo(Long variantId);
-}
