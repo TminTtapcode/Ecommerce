e78b7d31cd759ffe3518297b6d94bb3d71e4e0e4
TminTtapcode <oakceniki@gmail.com>
2026-09-01 12:37:25 +0700

    Delete EcomerceApp/order directory

--- EcomerceApp/order/entity/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package com.tmt.ecommerce.order.entity;
-
-public enum OrderStatus {
-    PENDING,    // Chờ xác nhận
-    CONFIRMED,  // Đã xác nhận, đang chuẩn bị hàng
-    SHIPPED,    // Đang giao hàng
-    DELIVERED,  // Đã giao thành công
-    CANCELLED   // Đã hủy
-}
